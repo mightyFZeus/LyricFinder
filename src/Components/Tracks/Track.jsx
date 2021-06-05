127268d5cc70d1a01f2afc46d387cf874b6930fd
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-06-05 04:50:26 -0700

    track details

--- src/Components/Tracks/Track.jsx
+++ src/Components/Tracks/Track.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-
+import {Link } from 'react-router-dom'
 const Track = ({track}) => {
   return (
     <>
       <div className='col-md-6'>
         <div className='card mb-4 shadow-sm'>
           <div className='card-body'>
-            <h1>{track.artist_name}</h1>
+            <h5>{track.artist_name}</h5>
+            <p className='card-text'>
+              <strong><i className='fa fa-play'>Track</i></strong>: {track.track_name}
+              <strong><i className='fa fa-compact-disc'>Album</i></strong>: {track.album_name}
+            </p>
+            <Link to={`lyrics/track${track.track_id}`} className='btn btn-dark btn-block'>
+              <i className='fas fa-chevron-right'></i>View lyrics
+            </Link>
           </div>
           
         </div>
