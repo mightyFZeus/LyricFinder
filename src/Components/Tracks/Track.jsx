1700a9bab04f4eeeb36467a39b4f95cd28819934
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-06-05 05:01:03 -0700

    still details

--- src/Components/Tracks/Track.jsx
+++ src/Components/Tracks/Track.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import {Link } from 'react-router-dom'
-const Track = ({track}) => {
+import { Link } from "react-router-dom";
+const Track = ({ track }) => {
   return (
     <>
-      <div className='col-md-6'>
-        <div className='card mb-4 shadow-sm'>
-          <div className='card-body'>
+      <div className="col-md-6">
+        <div className="card mb-4 shadow-sm">
+          <div className="card-body">
             <h5>{track.artist_name}</h5>
-            <p className='card-text'>
-              <strong><i className='fa fa-play'>Track</i></strong>: {track.track_name}
-              <strong><i className='fa fa-compact-disc'>Album</i></strong>: {track.album_name}
+            <p className="card-text">
+              <strong>
+                <i className="fa fa-play">Track</i>
+              </strong>
+              : {track.track_name}
+              <br />
+              <strong>
+                <i className="fa fa-compact-disc">Album</i>
+              </strong>
+              : {track.album_name}
             </p>
-            <Link to={`lyrics/track${track.track_id}`} className='btn btn-dark btn-block'>
-              <i className='fas fa-chevron-right'></i>View lyrics
+            <Link
+              to={`lyrics/track${track.track_id}`}
+              className="btn btn-dark btn-block"
+            >
+              <i className="fas fa-chevron-right"></i>View lyrics
             </Link>
           </div>
-          
         </div>
       </div>
     </>
