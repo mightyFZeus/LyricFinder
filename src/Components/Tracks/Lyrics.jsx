fe7b1315f77dbba611a2b485a03015b5485a5863
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-06-06 10:36:30 -0700

    lyrics page

--- src/Components/Tracks/Lyrics.jsx
+++ src/Components/Tracks/Lyrics.jsx
@@ -1,5 +1,7 @@
 import React,{useState, useEffect, useContext} from "react";
 import axios from 'axios'
+import {Link} from 'react-router-dom'
+import Spinner from '../Layout/Spinner'
 
 const Lyrics = (props) => {
   const [track, setTrack] = useState({})
@@ -13,14 +15,43 @@ const Lyrics = (props) => {
       )
       .then((res) => {
         console.log(res.data)
-        // setLyrics({ track_list: res.data.message.body.track_list });
+        let lyrics= res.data.message.body.lyrics
+         setLyrics({ lyrics });
+      
+      
+
+      return  axios.get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${ props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      })
+      .then(res =>{
+        console.log(res.data)
+        let track= res.data.message.body.track
+        setTrack({track})
       })
       .catch((err) => console.log(err));
    
   }, []);
   return (
     <>
-      <h1>Lyrics</h1>
+      {track ===undefined || lyrics === undefined ||
+       Object.keys(track).length ===0 ||
+        Object.keys(lyrics).length ===0? <Spinner /> :
+         <>
+            <Link to='/' className='btn btn-dark btn-sm mb-4'>Go back</Link>
+            <div className='card'>
+              <h5 className='card-header'>
+                {track.track_name} by <span className='text-secondary'>{track.artist_name}</span>
+              </h5>
+              <div className='card-body'>
+                <p cllasName='card-text'>
+                  {lyrics.lyrics_body}
+                </p>
+              </div>
+            </div>
+         </>
+      }
+  
     </>
   );
 };
