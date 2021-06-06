1194a08ca92a4f3ef4fe3ed0a39a1bc9ae3d18c7
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-06-06 06:16:11 -0700

    maybe

--- src/Components/Tracks/Track.jsx
+++ src/Components/Tracks/Track.jsx
@@ -19,7 +19,7 @@ const Track = ({ track }) => {
               : {track.album_name}
             </p>
             <Link
-              to={`lyrics/track${track.track_id}`}
+              to={`lyrics/track/${track.track_id}`}
               className="btn btn-dark btn-block"
             >
               <i className="fas fa-chevron-right"></i>View lyrics
